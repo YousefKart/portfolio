f69fc8d3a6c242e383cf8c8c8c9cbb2f74dbc50c
Yousef <karty@uwindsor.ca>
2024-12-02 20:34:02 -0500

    added skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,146 @@
-import Image from "next/image";
+import { HomeIcon } from "@heroicons/react/20/solid";
 
-export default function Home() {
+// Common Styles
+const commonStyles = "w-full flex flex-col items-center";
+const titleStyles =
+  "bg-bottom-teal-300 border-b-2 border-teal-300 text-4xl font-bold mt-16 mb-4";
+const listTextStyle = "text-right w-48 text-lg mr-4 mt-1";
+const listDiv1Style = "w-48 h-4 border-2 relative";
+const listDiv2Style = "h-4 bg-white absolute top-0 left-0";
+
+// Skill Item Component
+const SkillItem = ({ skill, width }: { skill: string; width: string }) => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <li className="flex">
+      <p className={listTextStyle}>{skill}</p>
+      <div className="relative mt-3 ml-4">
+        <div className={listDiv1Style} />
+        <div className={listDiv2Style} style={{ width }} />
+      </div>
+    </li>
+  );
+};
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+export default function SkillsList() {
+  const tools = [
+    { name: "VS Code", width: "90%" },
+    { name: "IntelliJ", width: "90%" },
+    { name: "PyCharm", width: "80%" },
+    { name: "Eclipse", width: "70%" },
+    { name: "Android Studio", width: "80%" },
+    { name: "Git", width: "80%" },
+    { name: "Postman", width: "70%" },
+    { name: "Docker", width: "60%" },
+    { name: "Jira", width: "60%" },
+    { name: "Slack", width: "60%" },
+  ];
+
+  const languages = [
+    { name: "Java", width: "100%" },
+    { name: "C/C++", width: "85%" },
+    { name: "Python", width: "80%" },
+    { name: "JavaScript", width: "60%" },
+    { name: "HTML/CSS", width: "70%" },
+    { name: "Kotlin", width: "90%" },
+    { name: "C#", width: "80%" },
+    { name: "SQL", width: "60%" },
+    { name: "MatLab", width: "60%" },
+    { name: "Lua", width: "50%" },
+  ];
+
+  const libraries = [
+    { name: "React", width: "80%" },
+    { name: "Node.js", width: "70%" },
+    { name: "Express", width: "70%" },
+    { name: "JQuery", width: "60%" },
+    { name: "Bootstrap", width: "70%" },
+    { name: "Tailwind", width: "80%" },
+    { name: "Material-UI", width: "70%" },
+    { name: "Socket.io", width: "60%" },
+    { name: "Django", width: "70%" },
+    { name: "Flask", width: "60%" },
+  ];
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* Navbar */}
+      <nav className="w-full bg-bottom-teal-300 border-b-2 border-teal-300 fixed top-0">
+        <ul className="flex justify-start space-between bg-neutral-800 w-full gap-8 pl-8">
+          <li>
+            <HomeIcon className="h-8 w-8 mt-3" />
+          </li>
+          <li className="p-3 text-2xl font-bold">Skills</li>
+          <li className="p-3 text-2xl font-bold">Projects</li>
+          <li className="p-3 text-2xl font-bold">Contact</li>
+        </ul>
+      </nav>
+
+      {/* Main Content */}
+      <div className="flex-grow flex flex-col items-center mt-12">
+        <header className={`${commonStyles} bg-neutral-900`}>
+          <h1 className={titleStyles}>YOUSEF KART</h1>
+          <h2 className="text-2xl font-bold mt-4 mb-8">
+            4th Year Computer Science Student
+          </h2>
+          <p className="w-2/5 text-center">
+            I have 60 years programming experience.
+          </p>
+        </header>
+
+        <main className={`${commonStyles} bg-neutral-900`}>
+          <div className={`${commonStyles}`}>
+            <h1 className={`${titleStyles} w-3/4 text-center`}>Skills</h1>
+            <div className="flex gap-16 space-between justify-center">
+              <div>
+                <h2 className="text-2xl font-bold text-center">Tools</h2>
+                <ul className="relative right-8">
+                  {tools.map((tool) => (
+                    <SkillItem
+                      key={tool.name}
+                      skill={tool.name}
+                      width={tool.width}
+                    />
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h2 className="text-2xl font-bold text-center">Languages</h2>
+                <ul className="relative right-8">
+                  {languages.map((language) => (
+                    <SkillItem
+                      key={language.name}
+                      skill={language.name}
+                      width={language.width}
+                    />
+                  ))}
+                </ul>
+              </div>
+              <div>
+                <h2 className="text-2xl font-bold text-center">Libraries</h2>
+                <ul className="relative right-8">
+                  {libraries.map((librarie) => (
+                    <SkillItem
+                      key={librarie.name}
+                      skill={librarie.name}
+                      width={librarie.width}
+                    />
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className={`${commonStyles}`}>
+            <h1 className={`${titleStyles} w-3/4 text-center`}>Projects</h1>
+          </div>
+          <div className={`${commonStyles}`}>
+            <h1 className={`${titleStyles} w-3/4 text-center`}>Contact</h1>
+          </div>
+        </main>
+      </div>
+
+      {/* Footer */}
+      <footer className="w-full bg-neutral-800 flex justify-center items-center py-4 border-t-2 border-teal-300">
+        <p>(c) 2024 Yousef Kart</p>
       </footer>
     </div>
   );
