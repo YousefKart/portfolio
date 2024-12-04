f6b91d207be7b01c48e1c8da2767c07baecfa1a7
Yousef <karty@uwindsor.ca>
2024-12-03 20:24:43 -0500

    added colour variables

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
 
 const commonStyles = 'w-full flex flex-col items-center';
-const titleStyles =
-  'bg-bottom-teal-300 border-b-2 border-teal-300 text-4xl font-bold mt-16 mb-4';
+const titleStyles = 'border-b-2 border-tertiary text-4xl font-bold mt-16 mb-4';
 const listTextStyle = 'text-right w-48 text-lg mr-4 mt-1';
 const listDiv1Style = 'w-48 h-4 border-2 relative';
 const listDiv2Style = 'h-4 bg-white absolute top-0 left-0';
@@ -62,27 +61,27 @@ export default function Home() {
   ];
 
   return (
-    <div className="flex flex-col min-h-screen"> 
+    <div className="flex flex-col min-h-screen">
       {/* Navbar */}
-      <nav className="flex w-full bg-bottom-teal-300 border-b-2 border-teal-300 fixed top-0 z-50">
-        <ul className="flex justify-start space-between bg-neutral-800 w-1/2 gap-8 pl-8">
+      <nav className="flex w-full border-b-2 border-tertiary fixed top-0 z-50">
+        <ul className="flex justify-start space-between bg-secondary w-1/2 gap-8 pl-8">
           <li>
             <HomeIcon className="h-8 w-8 mt-3" />
           </li>
-            <li className="p-3 text-2xl font-bold">
+          <li className="p-3 text-2xl font-bold">
             <a href="#skills">Skills</a>
-            </li>
-            <li className="p-3 text-2xl font-bold">
+          </li>
+          <li className="p-3 text-2xl font-bold">
             <a href="#projects">Projects</a>
-            </li>
-            <li className="p-3 text-2xl font-bold">
+          </li>
+          <li className="p-3 text-2xl font-bold">
             <a href="#models">Models</a>
-            </li>
-            <li className="p-3 text-2xl font-bold">
+          </li>
+          <li className="p-3 text-2xl font-bold">
             <a href="#contact">Contact</a>
-            </li>
+          </li>
         </ul>
-        <ul className="flex justify-end space-between bg-neutral-800 w-1/2 gap-8 pr-8">
+        <ul className="flex justify-end space-between bg-secondary w-1/2 gap-8 pr-8">
           <li>
             <a href="https://www.github.com/YousefKart" target="#">
               <FaGithub className="h-8 w-8 mt-3" />
@@ -98,7 +97,7 @@ export default function Home() {
 
       {/* Main Content */}
       <div className="flex-grow flex flex-col items-center mt-12">
-        <header className={`${commonStyles} bg-neutral-900`}>
+        <header className={`${commonStyles} bg-primary`}>
           <h1 className={titleStyles}>YOUSEF KART</h1>
           <h2 className="text-2xl font-bold mt-4 mb-8">
             4th Year Computer Science Student
@@ -107,8 +106,7 @@ export default function Home() {
             I have 60 years programming experience.
           </p>
         </header>
-        <main className={`${commonStyles} bg-neutral-900`}>
-
+        <main className={`${commonStyles} bg-primary`}>
           <div id="skills" className={`${commonStyles}`}>
             <h1 className={`${titleStyles} w-3/4 text-center`}>Skills</h1>
             <div className="flex gap-16 space-between justify-center">
@@ -182,12 +180,11 @@ export default function Home() {
               </li>
             </ul>
           </div>
-
         </main>
       </div>
 
       {/* Footer */}
-      <footer className="w-full bg-neutral-800 flex justify-center items-center py-4 border-t-2 border-teal-300">
+      <footer className="w-full bg-secondary flex justify-center items-center py-4 border-t-2 border-tertiary">
         <p>
           (c) 2024 Yousef Kart | [
           <a href="https://github.com/YousefKart/portfolio" target="#">
