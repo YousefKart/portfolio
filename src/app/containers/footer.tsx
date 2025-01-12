8380dca533230807a9d1787fe729c90eba63ff49
Yousef <karty@uwindsor.ca>
2025-01-12 13:00:10 -0500

    update bio

--- src/app/containers/footer.tsx
+++ src/app/containers/footer.tsx
@@ -1,13 +1,13 @@
 export default function Footer() {
-    return (
-        <footer className="w-full bg-secondary flex justify-center items-center py-4 border-t-2 border-tertiary">
-            <p>
-            (c) 2024 Yousef Kart | [
-            <a href="https://github.com/YousefKart/portfolio" target="#">
-                WIP
-            </a>
-            ]
-            </p>
-      </footer>
-    );
+  return (
+    <footer className="w-full bg-secondary flex justify-center items-center py-4 border-t-2 border-tertiary">
+      <p>
+        (c) 2025 Yousef Kart | [
+        <a href="https://github.com/YousefKart/portfolio" target="#">
+          WIP
+        </a>
+        ]
+      </p>
+    </footer>
+  );
 }
