e5e64dfdb6a7e40535391c8ab46d65737702d9c3
Yousef <karty@uwindsor.ca>
2025-01-28 19:33:21 -0500

    Updated Footer

--- src/app/containers/footer.tsx
+++ src/app/containers/footer.tsx
@@ -1,13 +1,10 @@
+import { FaRegCopyright } from 'react-icons/fa';
+
 export default function Footer() {
   return (
     <footer className="w-full bg-secondary flex justify-center items-center py-4 border-t-2 border-tertiary">
-      <p>
-        (c) 2025 Yousef Kart | [
-        <a href="https://github.com/YousefKart/portfolio" target="#">
-          WIP
-        </a>
-        ]
-      </p>
+      <FaRegCopyright className="mr-2" />
+      <p>2025 Yousef Kart</p>
     </footer>
   );
 }
