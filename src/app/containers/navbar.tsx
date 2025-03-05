c82fd05e4123cd59900d30380597b8970254f098
Yousef <karty@uwindsor.ca>
2025-03-04 19:38:44 -0500

    Revert "Added Hovering to Icons"
    This reverts commit 05724fdd1afe6d5110f26ab6511add0de098a9a8.

--- src/app/containers/navbar.tsx
+++ src/app/containers/navbar.tsx
@@ -2,9 +2,6 @@ import { useState, useEffect } from 'react';
 import { HomeIcon } from '@heroicons/react/20/solid';
 import { FaLinkedin, FaGithub, FaFileAlt } from 'react-icons/fa';
 
-const hover =
-  'hover:-translate-y-1 transform transition-transform duration-200';
-
 export default function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
 
@@ -26,7 +23,9 @@ export default function Navbar() {
   return (
     <nav
       className={`flex w-full fixed top-0 z-50 transition-colors duration-300 ${
-        isScrolled ? 'bg-secondary' : 'bg-transparent'
+        isScrolled
+          ? 'bg-secondary border-b-2 border-tertiary'
+          : 'bg-transparent border-b-2 border-transparent'
       }`}
     >
       <ul className="flex justify-start space-between w-1/2 gap-8 pl-8">
@@ -51,17 +50,17 @@ export default function Navbar() {
       <ul className="flex justify-end space-between w-1/2 gap-8 pr-8">
         <li>
           <a href="https://www.github.com/YousefKart" target="_blank">
-            <FaGithub className={`h-8 w-8 mt-3 ${hover}`} />
+            <FaGithub className="h-8 w-8 mt-3" />
           </a>
         </li>
         <li>
           <a href="https://www.linkedin.com/in/yousef-kart/" target="_blank">
-            <FaLinkedin className={`h-8 w-8 mt-3 ${hover}`} />
+            <FaLinkedin className="h-8 w-8 mt-3" />
           </a>
         </li>
         <li>
           <a href="/resume/Yousef%20Kart%20Resume.pdf" target="_blank">
-            <FaFileAlt className={`h-8 w-8 mt-3 ${hover}`} />
+            <FaFileAlt className="h-8 w-8 mt-3" />
           </a>
         </li>
       </ul>
