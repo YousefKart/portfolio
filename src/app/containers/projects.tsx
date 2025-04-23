f3f9dab21ec49a4ffea1728b77490fdba75ba4c1
BrosefFTW21 <BrosefFTW@gmail.com>
2025-04-23 13:05:07 -0400

    Update File Name

--- src/app/containers/projects.tsx
+++ src/app/containers/projects.tsx
@@ -68,7 +68,7 @@ const projects = [
       { icon: <FaLink />, url: 'https://volt.borhansaflo.com/' },
     ],
     media: [
-      'videos/VoltVistaClip.mp4',
+      'videos/VoltVistaCli.mp4',
       'images/projects/VoltVistaLogo.webp',
       'images/projects/VoltVistaHome.webp',
     ],
