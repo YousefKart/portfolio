fb7738e19034a78630cb3cfac77c13559a5402e0
Yousef <BrosefFTW@gmail.com>
2025-08-05 10:45:24 -0400

    add new experience

--- components/data/ExperienceData.ts
+++ components/data/ExperienceData.ts
@@ -11,6 +11,7 @@ export const experienceData: ExperienceData[] = [
       'Designed and implemented shared-authentication for SSO across the Scelta Suite.',
       'Developed and deployed a standardized UI-package for Scelta web applications.',
       'Created a NextJS app template to streamline web prototyping and testing.',
+      'Implemented a powerful and flexible feature for embedding 3D models on a web-based map.',
       'Collaborated with cross-functional teams to develop and maintain core features on 6 different applications (Scelta Home, REDMap, Sitekick, SpeedCRM, Portal, and Encore).',
       'Optimized database queries and API endpoints, reducing loading times and improving user experience.',
     ],
@@ -44,6 +45,33 @@ export const experienceData: ExperienceData[] = [
           'SQL',
           'C#',
         ],
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    title: '3D Modelling Lead',
+    company: 'Scelta',
+    location: 'Windsor, ON',
+    date: 'Feb 2025 - Present',
+    description: [
+      'Created 3D models of proposed developments using architectural blueprints, site plans and inspiration images.',
+      'Produced cost-effective, presentation-ready visualizations in 3–5 days, compared to the industry average of 2–4 weeks.',
+      'Enabled successful client pitches, directly contributing to increased sales of other products within the Scelta Suite.',
+    ],
+    tools: [
+      {
+        title: '3D Modelling',
+        tools: ['Blender'],
+      },
+      {
+        title: 'Collaboration',
+        tools: ['Trello', 'Google Drive'],
+      },
+      {
+        title: 'Languages',
+        tools: ['Python'],
       },
     ],
   },
