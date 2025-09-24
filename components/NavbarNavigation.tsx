17c18f126899cace3022e3d68047bd035b290623
Yousef <karty@uwindsor.ca>
2025-09-24 13:53:02 -0400

    update navbar hover behaviour

--- components/NavbarNavigation.tsx
+++ components/NavbarNavigation.tsx
@@ -3,19 +3,19 @@ import { NavbarLink } from './NavbarLink';
 export default function NavbarNavigation() {
   return (
     <div className="flex items-center justify-center gap-12 text-lg">
-      <NavbarLink href="#experience" label="experience">
+      <NavbarLink href="#experience" label="experience" scale={false}>
         <p>Experience</p>
       </NavbarLink>
 
-      <NavbarLink href="#projects" label="projects">
+      <NavbarLink href="#projects" label="projects" scale={false}>
         <p>Projects</p>
       </NavbarLink>
 
-      <NavbarLink href="#models" label="models">
+      <NavbarLink href="#models" label="models" scale={false}>
         <p>Models</p>
       </NavbarLink>
 
-      <NavbarLink href="#contact" label="contact">
+      <NavbarLink href="#contact" label="contact" scale={false}>
         <p>Contact</p>
       </NavbarLink>
     </div>
--- components/NavbarTitle.tsx
+++ components/NavbarTitle.tsx
@@ -2,7 +2,7 @@ import { NavbarLink } from './NavbarLink';
 
 export default function NavbarTitle() {
   return (
-    <NavbarLink href="#hero" label="Yousef Kart">
+    <NavbarLink href="#hero" label="Yousef Kart" scale={false}>
       <p className="text-lg font-semibold">Yousef Kart</p>
     </NavbarLink>
   );
